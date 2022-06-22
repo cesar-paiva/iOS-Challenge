c8abc27ec744f52b45b3ce8bce620297a1b6a981
cesar paiva <cesar.giupponi@gmail.com>
2022-06-22 00:55:39 -0300

    Add AppCoordinator

---
+++ UIKit/Movie Challenge/Movie Challenge/AppCoordinator.swift
@@ -0,0 +1,43 @@
+//
+//  AppCoordinator.swift
+//  Movie Challenge
+//
+//  Created by Cesar Paiva on 21/06/22.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController : UINavigationController { get set }
+
+    func start()
+    func childDidFinish(_ child: Coordinator?)
+}
+
+extension Coordinator {
+
+    func childDidFinish(_ child: Coordinator?) {
+
+        for (index, coordinator) in childCoordinators.enumerated() {
+
+            if coordinator === child {
+
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
+}
+
+class AppCoordinator: Coordinator {
+
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+
+    init(withNavigationController navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {}
+}
